67111d1debffa40bc2736569d66660226200ac10
marcsous <17042435+marcsous@users.noreply.github.com>
2021-06-12 11:59:04 -0700

    Add files via upload

--- test_nufft3d.m
+++ test_nufft3d.m
@@ -4,21 +4,20 @@ clear all
 %% object - 3D Cartesian object
 N = 128;
 im = phantom3d(N); % 3d shepp logan phantom
-%im(im==1) = i; % add phase to make it realistic
+im(im==1) = i; % add phase to make it realistic
 
 %% generate koosh ball data
-nInterleaves = 21;
-nRadialSpokes = 4200;
+nInterleaves = 21; % Fibonacci number
+nRadialSpokes = fix(5000 / nInterleaves) * nInterleaves;
 
-% a density adapted readout gradiect
-grad = 0.9 * [linspace(0,1,10) ones(1,N)];
+% a (kind of) density adapted readout 
+grad = [linspace(0,1,20) ones(1,20) linspace(1,0,N).^1.25];
 traj = cumsum(grad); traj = traj * (N/2-1) / max(traj);
 
 z = traj;
 y = zeros(size(z));
 x = zeros(size(z));
 
-% trajectory (units: phase cycles/fov)
 om = zeros(3,numel(traj),nRadialSpokes,'single');
 
 for k = 1:nRadialSpokes
@@ -26,7 +25,7 @@ for k = 1:nRadialSpokes
     % golden angle (http://blog.wolfram.com/2011/07/28/how-i-made-wine-glasses-from-sunflowers)
     dH = 1 - 2 * (k-1) / (nRadialSpokes-1);
     AzimuthalAngle(k) = acos(dH);
-    PolarAngle(k) = (k-1) * pi * (3 - sqrt(5));
+    PolarAngle(k) = max(k-1,0) * pi * (3 - sqrt(5));
     
     % rotation matrix
     RotationMatrix(1,1) = cos(PolarAngle(k))*cos(AzimuthalAngle(k));
@@ -49,25 +48,25 @@ k = reshape(1:nRadialSpokes,nInterleaves,[])';
 om = reshape(om(:,:,k),3,numel(traj),[],nInterleaves);
 
 %% create nufft object
-obj = nufft_3d(om,N,'radial',0);
+obj = nufft_3d(om,N);
 
 %% generate data (forward transform)
 data = obj.fNUFT(im);
-randn('state',0);
-noise = complex(randn(size(data)),randn(size(data))) * 100;
+noise = complex(randn(size(data)),randn(size(data))) * 10;
 data = data+noise;
 
 %% reconstruction (inverse transform)
-maxit = 1; % 1 for gridding, higher for conjugate gradient
+maxit = 10; % 0 or 1 for gridding, higher values for conjugate gradient
 damp = 0; % Tikhonov penalty on ||x||
-lambda = 0; % Tikhobov penalty on ||imag(x))||
-done = obj.iNUFT(data,maxit,damp,[],'phase-constraint',lambda);
+weight = []; % data weighting (optional)
+partial = 0.5; % Tikhobov penalty on ||imag(x))||
+im1 = obj.iNUFT(data,maxit); % plain reconstruction
+im2 = obj.iNUFT(data,maxit,damp,weight,'phase-constraint',partial);
 
 %% display
 subplot(1,4,1);
-plot(grad); title('density adapted readout gradient'); xlim([0 numel(grad)+1]);
-subplot(1,4,2);
 plot3(squeeze(om(1,end,:,1))',squeeze(om(2,end,:,1))',squeeze(om(3,end,:,1))','.');
 title('interleave 1'); grid on;
-subplot(1,4,3); imagesc(abs(im(:,:,N/2)),[0 0.25]); colorbar; title('original');
-subplot(1,4,4); imagesc(abs(done(:,:,N/2)),[0 0.25]); colorbar; title('radial');
+subplot(1,4,2); imagesc(abs(im(:,:,N/2)),[0 1]); colorbar; title('original');
+subplot(1,4,3); imagesc(abs(im1(:,:,N/2)),[0 1]); colorbar; title('radial');
+subplot(1,4,4); imagesc(abs(im2(:,:,N/2)),[0 1]); colorbar; title('radial+phase constraint');
